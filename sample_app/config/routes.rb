8649d7f9c573be1124c5c49f83a50d05472c70ef
Vo Tuan <votuan921@gmail.com>
2018-10-04 02:43:59 +0700

    finish homework

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get 'help', to: 'static_pages#help'
+  get 'sign_up', to: 'static_pages#home'
+  get 'about', to: 'static_pages#home'
+  get 'contact', to: 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
